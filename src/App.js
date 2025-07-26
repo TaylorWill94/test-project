a8d06c7588775937e4e3af541407b6f87351f078
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2025-07-25 21:36:34 -0400

    Added a component to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Store from "./components/Store";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, you can do it!</h1>
       <p>You will practice coding 30 mins per day</p>
+
+      <Routes>
+        <Route path="/" element={<Store />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Store.js
@@ -0,0 +1,5 @@
+function Store() {
+  return <h1>Hey, this is my test store</h1>;
+}
+
+export default Store;
