f37218f716ad7334afea2b2707eb4612f7f4d565
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2025-07-29 09:29:48 -0400

    Removes store to replace with home

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Store from "./components/Store";
 import Nav from "./components/Nav.js";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <h1>Hi, you can do it!</h1>
-      <p>You will practice coding 30 mins per day</p>
+      {/* <h1>Hi, you can do it!</h1>
+      <p>You will practice coding 30 mins per day</p> */}
 
       <Routes>
-        <Route path="/" element={<Store />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
