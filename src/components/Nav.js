28882c3f9f751171064c24cab48b3ae47bcd66aa
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2025-07-29 10:57:54 -0400

    Updates nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,14 @@
+import "./Nav.css";
+
 function Nav() {
-  return <h1>Navigation</h1>;
+  return (
+    <div className="nav-container">
+      <p>Home</p>
+      <p>Store</p>
+      <p>Contact</p>
+      <p>Motivation</p>
+    </div>
+  );
 }
 
 export default Nav;
