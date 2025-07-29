f53cdaae4739846eaac3b50ec404a71b9c62e5ac
Taylor <taylor@taylors-mbp.mynetworksettings.com>
2025-07-29 09:26:39 -0400

    added a new nav file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Store from "./components/Store";
+import Nav from "./components/Nav.js";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Hi, you can do it!</h1>
       <p>You will practice coding 30 mins per day</p>
 
---
+++ src/components/Nav.js
@@ -0,0 +1,5 @@
+function Nav() {
+  return <h1>Navigation</h1>;
+}
+
+export default Nav;
